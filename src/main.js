6ad166ee815df3e0763834faa10ca36f8b31e900
PEEPOOE <96319418+PEEPOOE@users.noreply.github.com>
2022-05-15 16:34:06 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 1) {
                 setInputError(inputElement, "Username must be at least 10 characters in length");
             }
         });
